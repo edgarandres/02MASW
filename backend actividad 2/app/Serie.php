7ad7856cac4b1024e6f99e7a6956d974d3c354f8
Andres Marin <andres.marin@kinesso.com>
2022-02-21 22:28:33 -0500

    Se ejecutan los seeders

--- backend actividad 2/app/Serie.php
+++ backend actividad 2/app/Serie.php
@@ -8,22 +8,22 @@ class Serie extends Model
 {
     //
     public function platform(){
-        return $this->hasOne(Platform::class)
+        return $this->hasOne(Platform::class);
     }
 
     public function director(){
-        return $this->hasOne(Director::class)
+        return $this->hasOne(Director::class);
     }
 
     public function actors(){
-        return $this->hasMany(Actor::class)
+        return $this->hasMany(Actor::class);
     }
 
     public function audioLaguage(){
-        return $this->hasMany(Audio::class)
+        return $this->hasMany(Audio::class);
     }
 
     public function subtitleLaguage(){
-        return $this->hasMany(Audio::class)
+        return $this->hasMany(Audio::class);
     }
 }
--- backend actividad 2/database/seeds/DatabaseSeeder.php
+++ backend actividad 2/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(ActorsTableSeeder::class);
+        $this->call(LanguagesTableSeeder::class);
+        $this->call(DirectorsTableSeeder::class);
+        $this->call(PlatformsTableSeeder::class);
     }
 }
